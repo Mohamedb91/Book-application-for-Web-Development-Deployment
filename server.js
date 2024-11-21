5fa814802ebe28a4a90b6c03695ba124b02a3122
Mohamed <mohamed.benyounis@gmail.com>
2024-11-21 17:25:03 +0000

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server
+// Start server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
